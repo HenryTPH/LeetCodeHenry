5a27381916698c319b05d6c2d6a7191d79432ce8
Henry Tu <hungtp2912@gmail.com>
2024-09-19 23:30:53 -0600

    Pass Course Schedule2

--- Java/LeetCode/src/CourseSchedule2.java
+++ Java/LeetCode/src/CourseSchedule2.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
 
 public class CourseSchedule2 {
     /**
@@ -13,9 +14,9 @@ Return the ordering of courses you should take to finish all courses. If there a
      * @param prerequisites
      * @return
      */
-    public int[] findOrder(int numCourses, int[][] prerequisites){
+    public static int[] findOrder(int numCourses, int[][] prerequisites){
         List<List<Integer>> graph = new ArrayList<>();
-        boolean[] visited = new boolean[numCourses];
+        int[] stateOfVisit = new int[numCourses];
         int[] rs = new int[numCourses];
         for(int i = 0; i < numCourses; i++){
             graph.add(new ArrayList<>());
@@ -24,9 +25,30 @@ Return the ordering of courses you should take to finish all courses. If there a
             graph.get(pos[1]).add(pos[0]);
             graph.get(pos[1]).sort((a, b) -> b-a);
         }
-         
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < numCourses; i++){
+            if(stateOfVisit[i] == 0 ){
+                if(checkCycle(i, graph, stateOfVisit, stack)) return new int[0];                
+            }
+        }
+        // if(stack.isEmpty()) return rs;
+        for(int i = 0; i < numCourses; i++){
+            rs[i] = stack.pop();
+        }
+        return rs;
+    }
+    public static boolean checkCycle(int node, List<List<Integer>> graph, int[] stateOfVisit, Stack<Integer> stack){
+        stateOfVisit[node] = 1;
+        for(int i: graph.get(node)){
+            if(stateOfVisit[i] == 1) return true;
+            if(stateOfVisit[i] == 0 && checkCycle(i, graph, stateOfVisit, stack)) return true;
+        }
+        stateOfVisit[node] = 2;
+        stack.push(node);
+        return false;
     }
     public static void main(String[] args) {
-        
+        int[][] arr = {{0, 1}, {1,0}};
+        System.out.println(findOrder(2, arr));
     }
 }
