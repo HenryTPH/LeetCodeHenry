db44e663bd254c9154f181523f9d87c90908b61b
Henry Tu <hungtp2912@gmail.com>
2024-10-10 17:33:46 -0600

    Another simple solution from Leatcode

--- Java/LeetCode/src/MinCostConnectPoints.java
+++ Java/LeetCode/src/MinCostConnectPoints.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -63,6 +64,40 @@ public class MinCostConnectPoints {
             current++;
         }
         return result;
+    }
+    /*
+     * Faster way on Leetcode
+     */
+    private static int distance(int[] a, int[] b){
+        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    }
+    public int minCostConnectPoints1(int[][] points){
+        int res = 0;
+        int nodes = points.length;
+        int[] distance = new int[nodes];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[nodes];
+        int current = 0;
+        distance[current] = 0;
+        for(int i = 0; i < nodes; i++){
+            int min = Integer.MAX_VALUE;
+            int bestPoint = -1;
+            for(int j = 0; j < nodes; j++){
+                if(!visited[j] && distance[j] < min){
+                    min = distance[j];
+                    bestPoint = j;
+                }
+            }
+            visited[bestPoint] = true;
+            res += min;
+            for(int k = 0; k < nodes; k++){
+                if(!visited[k]){
+                    int dist = distance(points[bestPoint], points[k]);
+                    distance[k] = Math.min(distance[k], dist);
+                }
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
         int[][] points = {{0,0},{2,2},{3,10},{5,2},{7,0}};
