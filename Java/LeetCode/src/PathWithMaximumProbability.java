0b9558f344ba271fa8cca6b8d46c17b4332e7426
Henry Tu <hungtp2912@gmail.com>
2024-10-15 16:29:16 -0600

    Tried to use Dijsktra Algorithm but ran into Time limit Exceeded

--- Java/LeetCode/src/PathWithMaximumProbability.java
+++ Java/LeetCode/src/PathWithMaximumProbability.java
@@ -1,4 +1,7 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class PathWithMaximumProbability {
     /*
@@ -27,8 +30,47 @@ If there is no path from start to end, return 0. Your answer will be accepted if
         }
         return distances[start_node][end_node];
     }
+    static class Edge {
+        int node;
+        double prob;
+        public Edge(int node, double prob){
+            this.node = node;
+            this.prob = prob;
+        }        
+    }
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node){
-        
+        LinkedList<Edge>[] list = new LinkedList[n];
+        for(int i = 0; i < n; i++){
+            list[i] = new LinkedList<>();
+        }
+        for(int e = 0; e < edges.length; e++){
+            int src = edges[e][0];
+            int dest = edges[e][1];
+            double prob = succProb[e];
+            list[src].add(new Edge(dest, prob));
+            list[dest].add(new Edge(src, prob));
+        }
+        PriorityQueue<Edge> pq = new PriorityQueue<>(n, Comparator.comparingDouble(e -> e.prob));
+        // boolean[] visited = new boolean[n];
+        double[] distances = new double[n];
+        Arrays.fill(distances, 0);
+        distances[start_node] = 1;
+        pq.add(new Edge(start_node, 0));
+        while(!pq.isEmpty()){
+            Edge curr = pq.poll();
+            int currentVertex = curr.node;
+            // if(visited[currentVertex]) continue;
+            // visited[currentVertex] = true;
+            for(Edge e: list[currentVertex]){
+                int neighbor = e.node;
+                double newProb = e.prob * distances[currentVertex];
+                if(Double.compare(newProb,distances[neighbor]) > 0){
+                    distances[neighbor] = newProb;
+                    pq.add(new Edge(neighbor, newProb));
+                }
+            }
+        }
+        return distances[end_node];
     }
     public static void main(String[] args) {
         
