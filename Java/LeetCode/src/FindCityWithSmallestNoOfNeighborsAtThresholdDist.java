b5e977b03fa4574d1fa78f6fb6a0561897b53331
Henry Tu <hungtp2912@gmail.com>
2024-10-18 11:30:09 -0600

    Solved using Bellman Ford Algorithm

--- Java/LeetCode/src/FindCityWithSmallestNoOfNeighborsAtThresholdDist.java
+++ Java/LeetCode/src/FindCityWithSmallestNoOfNeighborsAtThresholdDist.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class FindCityWithSmallestNoOfNeighborsAtThresholdDist {
     /*
      * There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
@@ -6,10 +8,57 @@ Return the city with the smallest number of cities that are reachable through so
 
 Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
      */
-    public int findTheCity(int n, int[][] edges, int distanceThreshold){
+    final static int INF = 99999;
+    public static int findTheCity(int n, int[][] edges, int distanceThreshold){
+        int[][] distances = new int[n][n];
+        int[] cities = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    distances[i][j] = 0;
+                } else {
+                    distances[i][j] = INF;
+                }
+            }
+        }
+        for(int i = 0; i < edges.length; i++){
+            int src = edges[i][0];
+            int dest = edges[i][1];
+            int weight = edges[i][2];
+            distances[src][dest] = weight;
+            distances[dest][src] = weight;
+        }
+        for(int k = 0; k < n; k++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){
+                    if(distances[i][k] + distances[k][j] < distances[i][j]){
+                        distances[i][j] = distances[i][k] + distances[k][j];
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && distances[i][j] <= distanceThreshold){
+                    cities[i]++;
+                }
+            }
+        }
+        int maxPosition = -1;
+        int maxValue = Integer.MAX_VALUE;
 
+        for (int i = 0; i < cities.length; i++) {
+            if (cities[i] < maxValue) {
+                maxValue = cities[i];
+                maxPosition = i;
+            } else if (cities[i] == maxValue) {
+                maxPosition = i; 
+            }
+        }
+        return maxPosition;
     }
     public static void main(String[] args) {
-        
+        int[][] edges = {{0,1,3},{1,2,1},{1,3,4},{2,3,1}};
+        System.out.println(findTheCity(4, edges, 4));
     }
 }
