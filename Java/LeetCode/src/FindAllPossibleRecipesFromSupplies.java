eca82df233c31951b6c8b4f3241e32c8e485c3d3
Henry Tu <hungtp2912@gmail.com>
2024-10-24 00:12:21 -0600

    New problem

---
+++ Java/LeetCode/src/FindAllPossibleRecipesFromSupplies.java
@@ -0,0 +1,15 @@
+public class FindAllPossibleRecipesFromSupplies {
+    /*
+     * You have inforamtion about n different recipes. You are given a string array recipes and a 2D string array ingredients.
+     * The ith recipe has the name recipesp[i], and you can create it if you have all the needed ingredients from ingredietnesp[i].
+     * Ingredients to a recipe may need to be created from other recipes (Ingredients[i] may contain sa string that is in recipes)
+     * A string array of supplies containing all the ingredietns that you initially have. You have an infinite supply of all of them.
+     * Return a list of all the recipes that you can create. Answer may be in any order
+     */
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
