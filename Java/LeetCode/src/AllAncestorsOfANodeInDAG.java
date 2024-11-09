cb16849f11bf180d46b58d8e5b8533d338e5a8d9
Henry Tu <hungtp2912@gmail.com>
2024-11-09 13:44:49 -0600

    Try to solve All Ancestors Of A Node In DAG

---
+++ Java/LeetCode/src/AllAncestorsOfANodeInDAG.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllAncestorsOfANodeInDAG {
+    public List<List<Integer>> getAncestors(int n, int[][] edges){
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
