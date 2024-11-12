f93ff866244776441f48c55b9d7bc04d7fb8d5c0
Henry Tu <hungtp2912@gmail.com>
2024-11-12 00:47:44 -0600

    Pass all the test but run into time limit exceeded

--- Java/LeetCode/src/AllAncestorsOfANodeInDAG.java
+++ Java/LeetCode/src/AllAncestorsOfANodeInDAG.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class AllAncestorsOfANodeInDAG {
     /*
@@ -11,19 +14,43 @@ Return a list answer, where answer[i] is the list of ancestors of the ith node, 
 
 A node u is an ancestor of another node v if u can reach v via a set of edges.
      */
-    public List<List<Integer>> getAncestors(int n, int[][] edges){
+    public static List<List<Integer>> getAncestors(int n, int[][] edges){
         List<List<Integer>> result = new ArrayList<>();
         List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0; i < n; i++){
             result.add(new ArrayList<>());
             graph.add(new ArrayList<>());
         }        
-        for(int i = 0; i > edges.length; i++){
+        for(int i = 0; i < edges.length; i++){
             graph.get(edges[i][0]).add(edges[i][1]);
+        }
+        for(int i = 0 ; i < n; i++){
+            List<Integer> list = new ArrayList<>();
+            boolean[] visited = new boolean[n];
+            dfs(result, graph, i, list, visited);          
+        }
+        for(List<Integer> list: result){
+            Collections.sort(list);
         }
         return result;
     }
+    private static void dfs(List<List<Integer>> result, List<List<Integer>> graph, int node, List<Integer> list, boolean[] visited){
+        if(visited[node] || graph.get(node).isEmpty()){
+            return;
+        }
+        visited[node] = true;
+        if(!list.contains(node) && !graph.get(node).isEmpty()) list.add(node);
+        for(int neighbor: graph.get(node)){
+            for(int n: list){
+                if(!result.get(neighbor).contains(n)){
+                    result.get(neighbor).add(n);
+                }
+            } 
+            dfs(result, graph, neighbor, new ArrayList<>(list), visited);
+        }
+    }
     public static void main(String[] args) {
-        
+        int[][] edges = {{0,3},{0,4},{1,3},{2,4},{2,7},{3,5},{3,6},{3,7},{4,6}};
+        System.out.println(getAncestors(8, edges));
     }
 }
