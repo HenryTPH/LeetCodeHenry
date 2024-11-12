0632ce3639eed41e087228b3d3d80ef7b0bffc56
Henry Tu <hungtp2912@gmail.com>
2024-11-12 01:05:22 -0600

    Change the version to use Set but still face the Time limit exceeded

--- Java/LeetCode/src/AllAncestorsOfANodeInDAG.java
+++ Java/LeetCode/src/AllAncestorsOfANodeInDAG.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Stack;
 
 public class AllAncestorsOfANodeInDAG {
@@ -25,29 +27,28 @@ A node u is an ancestor of another node v if u can reach v via a set of edges.
             graph.get(edges[i][0]).add(edges[i][1]);
         }
         for(int i = 0 ; i < n; i++){
-            List<Integer> list = new ArrayList<>();
+            Set<Integer> set = new HashSet<>();
             boolean[] visited = new boolean[n];
-            dfs(result, graph, i, list, visited);          
+            dfs(result, graph, i, set, visited);          
         }
         for(List<Integer> list: result){
             Collections.sort(list);
         }
         return result;
     }
-    private static void dfs(List<List<Integer>> result, List<List<Integer>> graph, int node, List<Integer> list, boolean[] visited){
-        if(visited[node] || graph.get(node).isEmpty()){
+    private static void dfs(List<List<Integer>> result, List<List<Integer>> graph, int node, Set<Integer> set, boolean[] visited){
+        if(visited[node]){
             return;
         }
         visited[node] = true;
-        if(!list.contains(node) && !graph.get(node).isEmpty()) list.add(node);
-        for(int neighbor: graph.get(node)){
-            for(int n: list){
-                if(!result.get(neighbor).contains(n)){
-                    result.get(neighbor).add(n);
-                }
-            } 
-            dfs(result, graph, neighbor, new ArrayList<>(list), visited);
+        for(int rs: set){
+            if(!result.get(node).contains(rs))result.get(node).add(rs);
         }
+        set.add(node);
+        for(int neighbor: graph.get(node)){
+            dfs(result, graph, neighbor, set, visited);
+        }
+        set.remove(node);
     }
     public static void main(String[] args) {
         int[][] edges = {{0,3},{0,4},{1,3},{2,4},{2,7},{3,5},{3,6},{3,7},{4,6}};
